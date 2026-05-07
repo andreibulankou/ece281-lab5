dc2adf1c9be7d2cef1323e30a7f60d1d4dea06bd
andreibulankou <C28Andrei.Bulankou@afacademy.af.edu>
2026-05-06 19:55:11 -0600

    fixed 10-3 error

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -69,7 +69,9 @@ begin
      --Flags
      o_flags(3) <= s_result(7); --N: negative
      o_flags(2) <= '1' when s_result = "00000000" else '0'; --Z: zero
-     o_flags(1) <= s_sum(8); --C: carry
+     o_flags(1) <= '1' when i_op = "000" and s_sum(8) = '1' else
+                   '1' when i_op = "001" and unsigned(i_A) < unsigned(i_B) else 
+                   '0';
      o_flags(0) <= '0'; --V: overflow 
                    
 end Behavioral;
