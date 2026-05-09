e364fd4c97a35584da2035ebab77387281b849df
andreibulankou <C28Andrei.Bulankou@afacademy.af.edu>
2026-05-08 23:07:29 -0600

    finished code

--- src/controller_fsm.vhd
+++ src/controller_fsm.vhd
@@ -38,8 +38,36 @@ entity controller_fsm is
 end controller_fsm;
 
 architecture FSM of controller_fsm is
-
+    type state_type is (S0, S1, S2, S3); --declare states
+    signal s_state : state_type := S0;
 begin
-
-
+    process(i_adv)
+    begin
+        if rising_edge(i_adv) then
+            if i_reset = '1' then
+                s_state <= S0;
+            else 
+                case s_state is
+                    when S0 =>
+                        s_state <= S1;
+                    when S1 =>
+                        s_state <= S2;
+                    when S2 => 
+                        s_state <= S3;
+                    when S3 =>
+                        s_state <= S0;
+                    when others =>
+                        s_state <= S0;
+                end case;
+            end if;
+           end if;
+          end process;
+       
+       with s_state select
+           o_cycle <= "0001" when S0,
+                      "0010" when S1,
+                      "0100" when S2,
+                      "1000" when S3,
+                      "0001" when others;
+                        
 end FSM;
